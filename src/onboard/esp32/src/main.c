25fd8b19c8589903a5cde30f1d8adab83cad66ef
Srinath Iyer <srinathiyer06@gmail.com>
2025-03-26 21:54:18 -0400

    main.c architecture laid out; fixed lib directory layout.

--- src/onboard/esp32/src/main.c
+++ src/onboard/esp32/src/main.c
@@ -5,15 +5,48 @@
 #include "freertos/task.h"
 #include <mpu6050.h>
 
+MPU6050 mpu6050;
+
 void app_main(){
-    printf("Hello World\n");
-    MPU6050 mpu6050;
-    init_mpu6050_converted_data(&mpu6050);
-    init_mpu6050();
-    while(1){
-        update_mpu6050_measurements(&mpu6050);
-        print_mpu6050_data(&mpu6050);
-        vTaskDelay(100 / portTICK_PERIOD_MS);
-    }
+    init_drone();
+    
+    xTaskCreate(get_imu_data, "Get IMU Data", 2048, NULL, 5, NULL);
+    xTaskCreate(sensor_fusion, "Sensor Fusion", 2048, NULL, 5, NULL);
+    xTaskCreate(pid_controllers, "PID Controllers", 2048, NULL, 5, NULL);
+    xTaskCreate(motor_control, "Motor Control", 2048, NULL, 5, NULL);
+    xTaskCreate(check_safety, "Check Safety", 2048, NULL, 5, NULL);
+    xTaskCreate(send_data_to_pi, "Send Data to Pi", 2048, NULL, 5, NULL);
 }
 
+void init_drone(){
+    // MPU6050:
+    init_mpu6050_converted_data(&mpu6050);
+    init_mpu6050();
+}
+
+void get_imu_data(){
+    TickType_t last_wake_time = xTaskGetTickCount();
+    update_mpu6050_measurements(&mpu6050);
+    // Log or print_mpu6050_data(&mpu6050);
+    vTaskDelayUntil(&last_wake_time, 10 / portTICK_PERIOD_MS);
+}
+
+void sensor_fusion(){
+    TickType_t last_wake_time = xTaskGetTickCount();
+}
+
+void pid_controllers(){
+    TickType_t last_wake_time = xTaskGetTickCount();
+}
+
+void motor_control(){
+    TickType_t last_wake_time = xTaskGetTickCount();
+}
+
+void check_safety(){
+    TickType_t last_wake_time = xTaskGetTickCount();
+}
+
+void send_data_to_pi(){
+    TickType_t last_wake_time = xTaskGetTickCount();
+}
